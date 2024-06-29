260e1c7f12c621fcde7c9053ba58384ec314b88e
Corey Baker <coreyearleon@icloud.com>
2024-06-28 23:17:08 -0700

    make more methods public

--- Sources/CareKitEssentials/Cards/Shared/CareKitEssentialView.swift
+++ Sources/CareKitEssentials/Cards/Shared/CareKitEssentialView.swift
@@ -46,6 +46,29 @@ public protocol CareKitEssentialView: View {
         with taskIDs: [String],
         on date: Date
     ) -> OCKEventQuery
+
+    /// Append an `OCKOutcomeValue` to an event's `OCKOutcome`.
+    /// - Parameters:
+    ///   - values: An array of `OCKOutcomeValue`'s to append.
+    /// - Throws: An error if the outcome values cannot be set.
+    /// - Note: Appends occur if an`OCKOutcome` currently exists for the event.
+    /// Otherwise a new `OCKOutcome` is created with the respective outcome values.
+    func appendOutcomeValues(
+        _ values: [OCKOutcomeValue],
+        event: OCKAnyEvent,
+        store: OCKAnyStoreProtocol
+    ) async throws
+
+    /// Set/Replace the `OCKOutcomeValue`'s of an event.
+    /// - Parameters:
+    ///   - values: An array of `OCKOutcomeValue`'s to save.
+    /// - Throws: An error if the outcome values cannot be set.
+    /// - Note: Setting `values` to an empty array will delete the current `OCKOutcome` if it currently exists.
+    func saveOutcomeValues(
+        _ values: [OCKOutcomeValue],
+        event: OCKAnyEvent,
+        store: OCKAnyStoreProtocol
+    ) async throws
 }
 
 public extension CareKitEssentialView {
@@ -90,24 +113,6 @@ public extension CareKitEssentialView {
         _ = try await careStore.addAnyOutcome(outcome)
     }
 
-    static func eventQuery(
-        with taskIDs: [String],
-        on date: Date
-    ) -> OCKEventQuery {
-        var query = OCKEventQuery(for: date)
-        query.taskIDs = taskIDs
-        return query
-    }
-}
-
-extension CareKitEssentialView {
-
-    /// Append an `OCKOutcomeValue` to an event's `OCKOutcome`.
-    /// - Parameters:
-    ///   - values: An array of `OCKOutcomeValue`'s to append.
-    /// - Throws: An error if the outcome values cannot be set.
-    /// - Note: Appends occur if an`OCKOutcome` currently exists for the event.
-    /// Otherwise a new `OCKOutcome` is created with the respective outcome values.
     func appendOutcomeValues(
         _ values: [OCKOutcomeValue],
         event: OCKAnyEvent,
@@ -129,11 +134,6 @@ extension CareKitEssentialView {
         return
     }
 
-    /// Set/Replace the `OCKOutcomeValue`'s of an event.
-    /// - Parameters:
-    ///   - values: An array of `OCKOutcomeValue`'s to save.
-    /// - Throws: An error if the outcome values cannot be set.
-    /// - Note: Setting `values` to an empty array will delete the current `OCKOutcome` if it currently exists.
     func saveOutcomeValues(
         _ values: [OCKOutcomeValue],
         event: OCKAnyEvent,
@@ -166,6 +166,18 @@ extension CareKitEssentialView {
         currentOutcome.values = values
         _ = try await store.updateAnyOutcome(currentOutcome)
     }
+
+    static func eventQuery(
+        with taskIDs: [String],
+        on date: Date
+    ) -> OCKEventQuery {
+        var query = OCKEventQuery(for: date)
+        query.taskIDs = taskIDs
+        return query
+    }
+}
+
+extension CareKitEssentialView {
 
     /// Create an outcome for an event with the given outcome values.
     /// - Parameters:
