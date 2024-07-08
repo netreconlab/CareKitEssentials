99732f405f849067529f71ad109576fa71037ebd
Corey Baker <coreyearleon@icloud.com>
2024-07-07 18:43:37 -0700

    Update protocol extenstion to OCKAnyOutcome

--- Sources/CareKitEssentials/Extensions/OCKAnyOutcome+Sequence.swift
+++ Sources/CareKitEssentials/Extensions/OCKAnyOutcome+Sequence.swift
@@ -1,5 +1,5 @@
 //
-//  OCKOutcome+Sequence.swift
+//  OCKAnyOutcome+Sequence.swift
 //  CareKitEssentials
 //
 //  Created by Corey Baker on 7/7/24.
@@ -8,24 +8,24 @@
 import CareKitStore
 import Foundation
 
-public extension Sequence where Element == OCKOutcome {
+public extension Sequence where Element: OCKAnyOutcome {
 
     /**
-     Returns the `OCKOutcome` sorted in order from newest to oldest with respect to a given `KeyPath`.
+     Returns the `OCKAnyOutcome` sorted in order from newest to oldest with respect to a given `KeyPath`.
      When necessary, can specify a cutoff value for the respective `KeyPath` to be less than or equal to.
 
-     - parameter keyPath: An optional `Comparable` `KeyPath` to sort the `OCKOutcome`'s by.
-     - parameter lessThanEqualTo: The value that the `keyPath` of all `OCKOutcome`'s should
+     - parameter keyPath: An optional `Comparable` `KeyPath` to sort the `OCKAnyOutcome`'s by.
+     - parameter lessThanEqualTo: The value that the `keyPath` of all `OCKAnyOutcome`'s should
      be less than or equal to. If this value is `nil`, the
-     - returns: Returns an array of `OCKOutcome` sorted from newest to oldest with respect to `keyPath`.
-     - throws: An error when the `keyPath` cannot be unwrapped for any of the `OCKOutcome` values
+     - returns: Returns an array of `OCKAnyOutcome` sorted from newest to oldest with respect to `keyPath`.
+     - throws: An error when the `keyPath` cannot be unwrapped for any of the `OCKAnyOutcome` values
      in the array.
      */
     func sortedNewestToOldest<V>(
         _ keyPath: KeyPath<Element, V?>,
         lessThanEqualTo value: V? = nil
     ) throws -> [Element] where V: Comparable {
-        let outcomes = try compactMap { outcome -> OCKOutcome?  in
+        let outcomes = try compactMap { outcome -> Element?  in
             guard let outcomeKeyValue = outcome[keyPath: keyPath] else {
                 throw CareKitEssentialsError.couldntUnwrapRequiredField
             }
@@ -52,19 +52,19 @@ public extension Sequence where Element == OCKOutcome {
     }
 
     /**
-     Returns the `OCKOutcome` sorted in order from newest to oldest with respect to a given `KeyPath`.
+     Returns the `OCKAnyOutcome` sorted in order from newest to oldest with respect to a given `KeyPath`.
      When necessary, can specify a cutoff value for the respective `KeyPath` to be less than or equal to.
 
-     - parameter keyPath: A `Comparable` `KeyPath` to sort the `OCKOutcome`'s by.
-     - parameter lessThanEqualTo: The value that the `keyPath` of all `OCKOutcome`'s should
+     - parameter keyPath: A `Comparable` `KeyPath` to sort the `OCKAnyOutcome`'s by.
+     - parameter lessThanEqualTo: The value that the `keyPath` of all `OCKAnyOutcome`'s should
      be less than or equal to. If this value is `nil`, the
-     - returns: Returns an array of `OCKOutcome` sorted from newest to oldest with respect to `keyPath`.
+     - returns: Returns an array of `OCKAnyOutcome` sorted from newest to oldest with respect to `keyPath`.
      */
     func sortedNewestToOldest<V>(
         _ keyPath: KeyPath<Element, V>,
         lessThanEqualTo value: V? = nil
     ) -> [Element] where V: Comparable {
-        let outcomes = compactMap { outcome -> OCKOutcome?  in
+        let outcomes = compactMap { outcome -> Element?  in
             let outcomeKeyValue = outcome[keyPath: keyPath]
 
             // If there's a value to compare to, check that the element
