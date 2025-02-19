103a5d145a9b34f97642bb268551ebe84601cfba
Corey Baker <coreyearleon@icloud.com>
2025-02-18 20:45:56 -0800

    add filter outcomeValues methods

---
+++ Tests/CareKitEssentialsTests/OCKOutcomeValueExtensionsTests.swift
@@ -0,0 +1,101 @@
+//
+//  OCKOutcomeValueExtensionsTests.swift
+//  CareKitEssentials
+//
+//  Created by Corey Baker on 2/18/25.
+//
+
+import XCTest
+import CareKitStore
+@testable import CareKitEssentials
+
+final class OCKOutcomeValueExtensionsTests: XCTestCase {
+
+    func testFilteredOutcomeValuesGreaterThanEqualToOptionalKeyPath() async throws {
+        // A date that represents 00h 00m 00s today or midnight.
+        let startOfDay = Calendar.current.startOfDay(for: Date())
+
+        // A date that represents 08h 00m 00s today or 8am.
+        let breakfastTime = Calendar.current.date(byAdding: .hour, value: 8, to: startOfDay)!
+
+        // A date that represents 13h 00m 00s or 1pm.
+        let lunchTime = Calendar.current.date(byAdding: .hour, value: 5, to: breakfastTime)!
+
+        // A date that represents 19h 30m 00s or 7.30pm.
+        let dinnerTime = Calendar.current.date(byAdding: .minute, value: 270, to: lunchTime)!
+
+        let stepTaskUUID = UUID()
+
+        var stepsAtBreakfast = OCKOutcomeValue(1000, units: "steps")
+        stepsAtBreakfast.startDate = breakfastTime
+        var stepsAtLunch = OCKOutcomeValue(2000, units: "steps")
+        stepsAtLunch.startDate = lunchTime
+        var stepsAtDinner = OCKOutcomeValue(3000, units: "steps")
+        stepsAtDinner.startDate = dinnerTime
+
+        let outcomeValues = [stepsAtBreakfast, stepsAtLunch, stepsAtDinner]
+        let stepsOutcome = OCKOutcome(
+            taskUUID: stepTaskUUID,
+            taskOccurrenceIndex: 1,
+            values: outcomeValues
+        )
+
+        XCTAssertEqual(stepsOutcome.values.count, 3)
+
+        let outcomeValuesAfterLunchInclusive = try stepsOutcome.values.filter(
+            \.startDate,
+             greaterThanEqualTo: lunchTime
+        )
+        XCTAssertEqual(outcomeValuesAfterLunchInclusive.count, 2)
+
+        let afterLunch = lunchTime + 1
+        let outcomesAfterLunch = try stepsOutcome.values.filter(
+            \.startDate,
+             greaterThanEqualTo: afterLunch
+        )
+        XCTAssertEqual(outcomesAfterLunch.count, 1)
+    }
+
+    func testFilteredOutcomeValuesGreaterThanEqualToRequiredKeyPath() {
+        // A date that represents 00h 00m 00s today or midnight.
+        let startOfDay = Calendar.current.startOfDay(for: Date())
+
+        // A date that represents 08h 00m 00s today or 8am.
+        let breakfastTime = Calendar.current.date(byAdding: .hour, value: 8, to: startOfDay)!
+
+        // A date that represents 13h 00m 00s or 1pm.
+        let lunchTime = Calendar.current.date(byAdding: .hour, value: 5, to: breakfastTime)!
+
+        // A date that represents 19h 30m 00s or 7.30pm.
+        let dinnerTime = Calendar.current.date(byAdding: .minute, value: 270, to: lunchTime)!
+
+        let stepTaskUUID = UUID()
+
+        var stepsAtBreakfast = OCKOutcomeValue(1000, units: "steps")
+        stepsAtBreakfast.createdDate = breakfastTime
+        var stepsAtLunch = OCKOutcomeValue(2000, units: "steps")
+        stepsAtLunch.createdDate = lunchTime
+        var stepsAtDinner = OCKOutcomeValue(3000, units: "steps")
+        stepsAtDinner.createdDate = dinnerTime
+        let outcomeValues = [stepsAtBreakfast, stepsAtLunch, stepsAtDinner]
+
+        let stepsOutcome = OCKOutcome(
+            taskUUID: stepTaskUUID,
+            taskOccurrenceIndex: 1,
+            values: outcomeValues
+        )
+
+        let outcomeValuesAfterLunchInclusive = stepsOutcome.values.filter(
+            \.createdDate,
+            greaterThanEqualTo: lunchTime
+        )
+        XCTAssertEqual(outcomeValuesAfterLunchInclusive.count, 2)
+
+        let afterLunch = lunchTime + 1
+        let outcomesAfterLunch = stepsOutcome.values.filter(
+            \.createdDate,
+             greaterThanEqualTo: afterLunch
+        )
+        XCTAssertEqual(outcomesAfterLunch.count, 1)
+    }
+}
