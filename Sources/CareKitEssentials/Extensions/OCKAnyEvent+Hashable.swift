65c9a872db9e41b555c6477e0ff737bd3019baab
Corey Baker <coreyearleon@icloud.com>
2025-05-02 12:58:00 -0700

    Better hash events related to HealthKit

--- Sources/CareKitEssentials/Extensions/OCKAnyEvent+Hashable.swift
+++ Sources/CareKitEssentials/Extensions/OCKAnyEvent+Hashable.swift
@@ -19,28 +19,32 @@ extension OCKEvent: Hashable where Task: CareKitEssentialVersionable, Outcome: C
 }
 
 extension OCKAnyEvent: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-        hasher.combine(scheduleEvent)
-        if let task = task as? OCKTask {
-            hasher.combine(task)
-        } else {
-            // OCKAnyTask
-            hasher.combine(task.id)
-            hasher.combine(task.title)
-            hasher.combine(task.instructions)
-            hasher.combine(task.impactsAdherence)
-            hasher.combine(task.schedule)
-            hasher.combine(task.groupIdentifier)
-            hasher.combine(task.uuid)
-            hasher.combine(task.asset)
-            hasher.combine(task.remoteID)
-            hasher.combine(task.notes)
-            hasher.combine(task.effectiveDate)
-        }
-        if let outcome = outcome as? OCKOutcome {
-            hasher.combine(outcome)
-        } else if let outcome {
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(id)
+		hasher.combine(scheduleEvent)
+		if let task = task as? OCKTask {
+			hasher.combine(task)
+		} else if let task = task as? OCKHealthKitTask {
+			hasher.combine(task)
+		} else {
+			// OCKAnyTask
+			hasher.combine(task.id)
+			hasher.combine(task.title)
+			hasher.combine(task.instructions)
+			hasher.combine(task.impactsAdherence)
+			hasher.combine(task.schedule)
+			hasher.combine(task.groupIdentifier)
+			hasher.combine(task.uuid)
+			hasher.combine(task.asset)
+			hasher.combine(task.remoteID)
+			hasher.combine(task.notes)
+			hasher.combine(task.effectiveDate)
+		}
+		if let outcome = outcome as? OCKOutcome {
+			hasher.combine(outcome)
+		} else if let outcome = outcome as? OCKHealthKitOutcome {
+			hasher.combine(outcome)
+		} else if let outcome {
             // OCKAnyOutcome
             hasher.combine(outcome.id)
             hasher.combine(outcome.taskUUID)
