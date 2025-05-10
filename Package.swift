f090e4f4330e07227f660841019434826acf06bf
Corey Baker <coreyearleon@icloud.com>
2025-05-09 18:06:20 -0700

    Bump to latest CareKit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/cbaker6/CareKit.git",
-            .upToNextMajor(from: "3.0.1")
+            .upToNextMajor(from: "3.0.3")
         )
     ],
     targets: [
