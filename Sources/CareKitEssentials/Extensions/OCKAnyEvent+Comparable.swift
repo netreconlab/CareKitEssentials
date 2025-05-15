776c82861c6a49095f2d90cdf6fe8439282e1ec6
Corey <coreyearleon@icloud.com>
2025-05-15 09:33:30 -0700

    fix: Use Event comparable from CareKit (#65)
    * Update OCKAnyEvent+Comparable.swift
    
    * Make OCKAnyEvent follow PartialEvent comparable

--- Sources/CareKitEssentials/Extensions/OCKAnyEvent+Comparable.swift
+++ Sources/CareKitEssentials/Extensions/OCKAnyEvent+Comparable.swift
@@ -8,14 +8,29 @@
 
 import CareKitStore
 
-extension OCKEvent: Comparable where Task: CareKitEssentialVersionable, Outcome: CareKitEssentialVersionable {
-	public static func < (lhs: OCKEvent, rhs: OCKEvent) -> Bool {
-		lhs.scheduleEvent < rhs.scheduleEvent
-	}
-}
-
 extension OCKAnyEvent: Comparable {
-    public static func < (lhs: OCKAnyEvent, rhs: OCKAnyEvent) -> Bool {
-		lhs.scheduleEvent < rhs.scheduleEvent
-    }
+
+	func isOrderedBefore(other: OCKAnyEvent) -> Bool {
+
+		if scheduleEvent.start != other.scheduleEvent.start {
+			return scheduleEvent.start < other.scheduleEvent.start
+		}
+
+		if task.uuid != other.task.uuid {
+			return task.uuid.uuidString < other.task.uuid.uuidString
+		}
+
+		// At this point, the two events belong to the same task version and occur at the same time. Sort by occurrence
+		// which should be guaranteed to be increasing between two events for the same task version.
+
+		if scheduleEvent.occurrence == other.scheduleEvent.occurrence {
+			assertionFailure("Unexpectedly found two events for the same task version with equal occurrences")
+		}
+
+		return scheduleEvent.occurrence < other.scheduleEvent.occurrence
+	}
+
+	public static func < (lhs: Self, rhs: Self) -> Bool {
+		lhs.isOrderedBefore(other: rhs)
+	}
 }
